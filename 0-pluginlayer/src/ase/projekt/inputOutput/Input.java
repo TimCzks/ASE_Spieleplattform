1c57664748e4719f17df38b3b2c46a50f4c73aad
Tim Czerkas <85937078+TimCzks@users.noreply.github.com>
2022-11-24 12:24:35 +0100

    Update Input.java

--- 0-pluginlayer/src/ase/projekt/inputOutput/Input.java
+++ 0-pluginlayer/src/ase/projekt/inputOutput/Input.java
@@ -1,5 +1,20 @@
 package ase.projekt.inputOutput;
 
 public class Input {
-
+  public String[] leseUserDateiEin(String username) {
+      File f = new File("C:\\ASE_Spieleplattform\\" + username + ".txt");
+      String[] s = null;
+      try {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        while (br.ready()) {
+          s = br.readLine().split(",");
+        }
+        br.close();
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return s;
+    }
 }
