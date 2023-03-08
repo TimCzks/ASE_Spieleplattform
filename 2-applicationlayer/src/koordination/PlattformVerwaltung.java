558d324e09ce541831b16ef4f4be9877831bd4fd
TimCzks <t.handy@gmx.de>
2023-03-08 09:59:51 +0100

    Refactoring: extract methods

--- 2-applicationlayer/src/koordination/PlattformVerwaltung.java
+++ 2-applicationlayer/src/koordination/PlattformVerwaltung.java
@@ -19,11 +19,7 @@ public class PlattformVerwaltung {
 	}
 
 	public void starteSpieleplattform(User user, Scanner sc) {
-		System.out.println("Hey, mit 'GGM' startest du eine Runde 'Galgenmännchen'."
-				+ "\nMit 'SSP' startest du eine Runde 'Schere, Stein, Papier'."
-				+ "\nMit 'ZR' startest du eine Runde 'Zahlen Raten'.\nMit dem Befehl 'STATS' kannst du dir deine bisherige Spielestatistik anschauen."
-				+ "\n�ber 'SAVE' kannst du deinen Account mit der aktuellen Statistik abspeichern."
-				+ "\n�ber 'EXIT' kannst du das Spiel verlassen - denke daran, vorher zu speichern.");
+		printEinleitungZuASE();
 		boolean inLoop = true;
 		do {
 
@@ -31,31 +27,13 @@ public class PlattformVerwaltung {
 			String eingabe = sc.next();
 			switch (eingabe) {
 			case "GGM":
-				ggm = new Galgenmaennchen(sc);
-				ggm.starteSpiel();
-				user.getStats()
-						.setSiegeGGM(user.getStats().getSiegeGGM() + ggm.validiereSpielergebnis(ggm.getLastingTries()));
-				user.getStats().setGespielteSpiele(user.getStats().getGespielteSpiele() + 1);
+				spieleGalgenmaennchen(user, sc);
 				break;
 			case "ZR":
-				zr = new ZahlenRaten(sc);
-				zr.startGame();
-				int spielergebnis = zr.validiereSpielergebnis();
-				user.getStats().setGespielteSpiele(user.getStats().getGespielteSpiele() + 1);
-				if (user.getStats().getRekordZR() > spielergebnis) {
-					user.getStats().setRekordZR(spielergebnis);
-				}
+				spieleZahlenRaten(user, sc);
 				break;
 			case "SSP":
-				ssp = new SchereSteinPapier(sc);
-				ssp.startGame();
-				user.getStats().setSiegeSSP(user.getStats().getSiegeSSP() + ssp.validiereSpielergebnis()[1]);
-				user.getStats()
-						.setNiederlagenSSP(user.getStats().getNiederlagenSSP() + ssp.validiereSpielergebnis()[2]);
-				user.getStats()
-						.setUnentschiedenSSP(user.getStats().getUnentschiedenSSP() + ssp.validiereSpielergebnis()[3]);
-				user.getStats()
-						.setGespielteSpiele(user.getStats().getGespielteSpiele() + ssp.validiereSpielergebnis()[0]);
+				spieleSchereSteinPapier(user, sc);
 				break;
 			case "STATS":
 				System.out.println(user.toString());
@@ -72,6 +50,40 @@ public class PlattformVerwaltung {
 				System.out.println("Eingabe nicht erkannt. Bitte achte auf die Schreibweise und versuche es erneut.");
 			}
 		} while (inLoop);
+	}
+
+	private void printEinleitungZuASE() {
+		System.out.println("Hey, mit 'GGM' startest du eine Runde 'Galgenmännchen'."
+				+ "\nMit 'SSP' startest du eine Runde 'Schere, Stein, Papier'."
+				+ "\nMit 'ZR' startest du eine Runde 'Zahlen Raten'.\nMit dem Befehl 'STATS' kannst du dir deine bisherige Spielestatistik anschauen."
+				+ "\nüber 'SAVE' kannst du deinen Account mit der aktuellen Statistik abspeichern."
+				+ "\nüber 'EXIT' kannst du das Spiel verlassen - denke daran, vorher zu speichern.");
+	}
+
+	private void spieleGalgenmaennchen(User user, Scanner sc) {
+		ggm = new Galgenmaennchen(sc);
+		ggm.starteSpiel();
+		user.getStats().setSiegeGGM(user.getStats().getSiegeGGM() + ggm.validiereSpielergebnis(ggm.getLastingTries()));
+		user.getStats().setGespielteSpiele(user.getStats().getGespielteSpiele() + 1);
+	}
+
+	private void spieleZahlenRaten(User user, Scanner sc) {
+		zr = new ZahlenRaten(sc);
+		zr.startGame();
+		int spielergebnis = zr.validiereSpielergebnis();
+		user.getStats().setGespielteSpiele(user.getStats().getGespielteSpiele() + 1);
+		if (user.getStats().getRekordZR() > spielergebnis) {
+			user.getStats().setRekordZR(spielergebnis);
+		}
+	}
+
+	private void spieleSchereSteinPapier(User user, Scanner sc) {
+		ssp = new SchereSteinPapier(sc);
+		ssp.startGame();
+		user.getStats().setSiegeSSP(user.getStats().getSiegeSSP() + ssp.validiereSpielergebnis()[1]);
+		user.getStats().setNiederlagenSSP(user.getStats().getNiederlagenSSP() + ssp.validiereSpielergebnis()[2]);
+		user.getStats().setUnentschiedenSSP(user.getStats().getUnentschiedenSSP() + ssp.validiereSpielergebnis()[3]);
+		user.getStats().setGespielteSpiele(user.getStats().getGespielteSpiele() + ssp.validiereSpielergebnis()[0]);
 	}
 
 }
